51834d2a9ad5d60dc8d40b989defcb167f5c0703
Igor Mayachenkov <igor.mayachenkov@gmail.com>
2022-11-02 13:52:37 +0300

    backend; auth loading

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
+import React,{useState,useEffect} from "react"
 import logo from './logo.svg';
 import './App.css';
+import {backend} from './Backend'
+
 
 function App() {
+  //console.log(`App render`,process.env)
   console.log(`App render`)
+
+  const [error,setError] = useState(null)
+  const [user, setUser]  = useState(null)
+
+  useEffect(() => {
+    console.log(`App mounted`)
+    async function fetchData(){
+      try{
+        backend.init()
+
+        // Request auth
+        const auth = await backend.request('GET','/auth/info')
+        setUser({
+          name : auth.user.name,
+          role : auth.role.name
+        })
+
+      }catch(err){
+        console.error(err)
+        setError(err)
+      }
+    }
+    fetchData()
+
+  }, []);
 
   return (
     <div className="App">
@@ -10,11 +39,21 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <span className='title'>Cameras Dashboard</span>
+        {user!=null && <div className="user" >
+          <div>{user.name}</div>
+          <div>{user.role}</div>
+        </div>}
       </header>
 
       <div id="layout-list"></div>
 
       <div id="layout-data"></div>
+
+      {error &&
+        <div className="error">
+          <div className="message">{error.toString()}</div>
+        </div>
+      }
 
 
     </div>
---
+++ src/Backend.js
@@ -0,0 +1,56 @@
+export class ApiError{
+    constructor(status,message){
+        this.status  = status
+        this.message = message
+    }
+    toString(){
+        return `API error: ${this.status} - ${this.message}`
+    }
+}
+export class Backend{
+        init(){
+        // Workspace
+        const urlParams = new URLSearchParams(window.location.search)
+        let ws = urlParams.get('ws')
+        if(!ws) throw('workspace ("ws") is not present in the window query')
+        
+        // Backend URL 
+        let protocol = window.location.protocol
+        let port     = protocol==='http:' ? 8080 : 8443;
+        this.API_URL = `${protocol}//${window.location.hostname}:${port}/${ws}`
+
+        console.log('Backend.init API_URL: '+this.API_URL);
+    }
+
+    //----------------------------------------------------------------
+    // SEND JSON
+    async request(method, path, json){
+        let params = {
+            method: method,
+            credentials: "include",// to allow cookies
+            headers: { }
+        }
+        if(json){
+            params.body = JSON.stringify(json)
+            params.headers['Content-Type'] = 'application/json';
+        }
+
+        return new Promise((resolve, reject) => {
+            let status = null
+
+            fetch( this.API_URL + path, params)
+            .then(response => {
+                status = response.status
+                return response.json()
+            })
+            .then(data => {
+                if(status===200) resolve(data)
+                else reject(new ApiError(status, data.message))
+            })
+            .catch(error => {reject(error)})
+        })
+    }
+}
+
+export const backend = new Backend()
+
