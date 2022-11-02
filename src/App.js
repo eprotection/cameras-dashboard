5d001cf0d2c6a63ed27352de18142d50545258a4
Igor Mayachenkov <igor.mayachenkov@gmail.com>
2022-11-02 11:43:43 +0300

    main responsive  layout done

--- src/App.js
+++ src/App.js
@@ -2,22 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  console.log(`App render`)
+
   return (
     <div className="App">
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <span className='title'>Cameras Dashboard</span>
       </header>
+
+      <div id="layout-list"></div>
+
+      <div id="layout-data"></div>
+
+
     </div>
   );
 }
